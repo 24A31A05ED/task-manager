bfe1b7363efb30a582049c7f26efcd4a1c90a889
Yamini Lakshmi Kantamsetti <yaminilakshmikantamsetti@gmail.com>
2026-05-23 18:56:49 +0530

    Task Management Project completed

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+// Routes
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Task Manager API Running");
+});
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+// Server
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
